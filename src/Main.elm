fa8ed5956db0a74df15d17f82c12cf49ecc2d669
Natxo <icabrebarrera@magento.com>
2016-08-14 20:30:16 +0200

    top type signatures and basic model

--- src/Main.elm
+++ src/Main.elm
@@ -1,16 +1,20 @@
 import Html.App exposing (beginnerProgram)
-import Html exposing (text, button)
+import Html exposing (Html, text, button)
 import Html.Events exposing (onClick)
 import Debug exposing (log)
 
 
+type alias Model = Int
+
 type Msg = Clicked
 
 
+main : Program Never
 main =
     beginnerProgram { model = 0, view = view, update = update }
 
 
+update : Msg -> Model -> Model
 update msg model =
     case msg of
         Clicked ->
@@ -19,6 +23,6 @@ update msg model =
             in
                 model
 
-
+view : Model -> Html Msg
 view model =
     button [ onClick Clicked ] [ text "click" ]
